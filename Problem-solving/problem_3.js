6a79d71d586988664b4c212765da449c236f894f
RUHANI77 <ruhaniakter1234@gmail.com>
2022-08-05 00:53:37 +0600

    problem_3

--- Problem-solving/problem_3.js
+++
@@ -1,6 +0,0 @@
-var string1 = "git push -u origin main";
-var string2 = " ";
-var string3 = "an awesome web developer";
-var resultString = string1.concat(string2,string3);
-
-console.log(resultString);
---
+++ problem_3.js
@@ -0,0 +1,5 @@
+const str1 = "I am going to be";
+const str3 = "an awesome web developer";
+const str = str1 + ' ' + str3;
+
+console.log(str);
